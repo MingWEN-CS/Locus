53eb3ef4eb94ed2ef2ed42d5a577ea74153dd87d
justinwm <justinwm@163.com>
2016-10-29 15:55:54 +0800

    add new source files

---
+++ src/miningChanges/FinalResultsAndRanking.java
@@ -0,0 +1,152 @@
+package miningChanges;
+
+
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import generics.Bug;
+import generics.Pair;
+import utils.FileToLines;
+import utils.ReadBugsFromXML;
+
+public class FinalResultsAndRanking {
+	public String loc = main.Main.settings.get("workingLoc");
+	private HashMap<Integer,HashSet<String>> inducingRevisions;
+	private HashMap<Integer,List<HashSet<String>>> potentialRevisions;
+	private HashMap<String,Long> revisionTime;
+	public List<Bug> bugs;
+	HashMap<Integer, HashMap<String,Double>> hunkResults;
+	
+	public void loadOracles() {
+		String filename = main.Main.changeOracle;
+		List<String> lines = FileToLines.fileToLines(filename);
+		int index = 0;
+		int depth = 3;
+		
+		bugs = ReadBugsFromXML.getFixedBugsFromXML(main.Main.settings.get("bugReport"));
+		
+		inducingRevisions = new HashMap<Integer,HashSet<String>>();
+		potentialRevisions = new HashMap<Integer,List<HashSet<String>>>();
+		while (index < lines.size()) {
+//			System.out.println(lines.get(index));
+			String[] splits = lines.get(index).split("\t");
+			int bucketId = Integer.parseInt(splits[0].trim());
+			String inducings = splits[1].substring(1, splits[1].length() - 1);
+			
+			splits = inducings.split(",");
+			inducingRevisions.put(bucketId, new HashSet<String>());
+			for (int i = 0; i < splits.length; i++)
+				inducingRevisions.get(bucketId).add(splits[i].trim());
+			index++;
+			index++;
+			
+			potentialRevisions.put(bucketId, new ArrayList<HashSet<String>>());
+			for (int i = 0; i <= depth; i++) {
+				index++;
+				potentialRevisions.get(bucketId).add(new HashSet<String>());
+				String line = lines.get(index);
+				line = line.substring(1, line.length() - 1);
+				splits = line.split(",");
+	//			System.out.println(bucketId + "\t" + splits.length);	
+				for (int j = 0; j < splits.length; j++)
+					potentialRevisions.get(bucketId).get(i).add(splits[j].trim());
+			}
+			index++;
+		}
+	}
+	
+	public void loadResults() {
+		String filename = loc + File.separator + "results.txt";
+		List<String> lines = FileToLines.fileToLines(filename);
+		hunkResults = new HashMap<Integer, HashMap<String,Double>>();
+		System.out.println(filename);
+		for (String line : lines) {
+			String[] split = line.split("\t");
+			int bid = Integer.parseInt(split[0]);
+			hunkResults.put(bid, new HashMap<String,Double>());
+			for (int i = 1; i < split.length; i++) {
+				hunkResults.get(bid).put(split[i].split(":")[0], Double.parseDouble(split[i].split(":")[1]));
+			}
+		}
+	}
+	
+	public void loadRevisionTime() throws ParseException {
+		List<String> lines = FileToLines.fileToLines(main.Main.settings.get("revisionInfo"));
+		revisionTime = new HashMap<String, Long>();
+		for (String line : lines) {
+			String[] splits = line.split("\t");
+			String revisionNO = splits[0];
+			Date date = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy Z").parse(splits[4]);
+			revisionTime.put(revisionNO, date.getTime());
+		}
+	}
+	
+	public void integrateResults() {
+		List<List<Integer>> ranks = new ArrayList<List<Integer>>();
+		for (Bug bug : bugs) {
+			int bid = bug.id;
+		
+			HashSet<String> potentialChanges = potentialRevisions.get(bid).get(0);
+			List<Pair<String, Long>> changeRanks = new ArrayList<Pair<String,Long>>();
+			for (String change : potentialChanges) {
+				if (revisionTime.containsKey(change))
+					changeRanks.add(new Pair<String,Long>(change, revisionTime.get(change)));
+				else changeRanks.add(new Pair<String, Long>(change, Long.MAX_VALUE));
+			}
+			Collections.sort(changeRanks);
+			HashMap<String, Double> timeScore = new HashMap<String,Double>();
+			for (int i = 0; i < changeRanks.size(); i++) {
+				int index = changeRanks.size() - i - 1;
+				timeScore.put(changeRanks.get(index).getKey(), 1.0 / (i + 1));
+			}
+				
+			HashMap<String,Double> results = hunkResults.get(bid);
+			
+			for (String change : results.keySet()) {
+				if (timeScore.containsKey(change))
+					results.put(change, results.get(change) + 0.2 * timeScore.get(change));
+				else results.put(change, 0.0);
+			}
+			
+			List<Pair<String, Double>> finalRanks = new ArrayList<Pair<String,Double>>();
+			for (String change : results.keySet()) {
+				finalRanks.add(new Pair<String,Double>(change, results.get(change)));
+			}
+			
+			Collections.sort(finalRanks);
+			List<Integer> rank = new ArrayList<Integer>();
+			
+			for (int i = 0; i < finalRanks.size(); i++) {
+				int index = finalRanks.size() - i - 1;
+				if (inducingRevisions.get(bid).contains(finalRanks.get(index).getKey()))
+					rank.add(i);
+			}
+			ranks.add(rank);
+			System.out.println(rank.toString());
+		}
+		
+		int N = 10;
+		double[] topN = EvaluationMetric.topN(ranks, N);
+		double map = EvaluationMetric.MAP(ranks);
+		double mrr = EvaluationMetric.MRR(ranks);
+		
+		System.out.println(map + "\t" + mrr);
+		for (int i = 0; i < N; i++) {
+			System.out.print(topN[i] + "\t");
+		}
+		System.out.println();
+	}
+	
+	public void getFinalResults() throws ParseException {
+		loadOracles();
+		loadResults();
+		loadRevisionTime();
+		integrateResults();
+	}
+}
